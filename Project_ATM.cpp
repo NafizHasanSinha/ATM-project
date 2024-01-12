57067a717c16ed2ea7c77ec9e6917810fd1f8040
NafizHasanSinha <154341736+NafizHasanSinha@users.noreply.github.com>
2024-01-11 18:29:49 -0800

    Update Project_ATM.cpp

--- Project_ATM.cpp
+++ Project_ATM.cpp
@@ -53,34 +53,7 @@ class ATM
             newnode->prev = NULL;
             head = newnode;
 
-            cout << "Account Created" << endl;
-        }
-        else
-        {
-            node *newnode = new node;
-            cout << "Please Enter your name: ";
-            cin >> x;
-            newnode->name = x;
-
-            cout << "Please enter your Card No: ";
-            cin >> y;
-            newnode->cardNo = y;
-
-            cout << "Please enter your Pin: ";
-            cin >> z;
-            newnode->pin = z;
-
-            newnode->cash = 0;
-
-            newnode->next = NULL;
-            node *temp = head;
-            while (temp->next != NULL)
-            {
-                temp = temp->next;
-            }
-            temp->next = newnode;
-            newnode->prev = temp;
-            cout << "Your Account created" << endl;
+            cout << "Your Account Created" << endl;
         }
     }
 
