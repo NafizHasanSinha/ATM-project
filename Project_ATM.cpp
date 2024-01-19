d2d8176644e7272da41707181513d1908c602862
NafizHasanSinha <154341736+NafizHasanSinha@users.noreply.github.com>
2024-01-18 20:52:16 -0800

    Deposit& User menu.

--- Project_ATM.cpp
+++ Project_ATM.cpp
@@ -75,21 +75,72 @@ class ATM
             }
             temp = temp->next;
         }
+
         cout << "Your cardNo or Pin is wrong\n";
         cout << "Please Try Again....\n\n";
         return 1;
     }
 
+    void Deposit()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
+            if (temp->cardNo == y && temp->pin == z)
+            {
+                cout << "Please Enter The Amount: ";
+                cin >> c;
+                temp->cash = temp->cash + c;
+                cout << "Amount successfully Added" << endl;
+                break;
+            }
+            temp = temp->next;
+        }
+    }
+    void user_menu()
+    {
+        cout << "_____________________" << endl;
+        cout << "       USER MENU" << endl;
+        cout << " 1. Deposit Money" << endl;
+        cout << " 2. Withdraw Money" << endl;
+        cout << " 3. Check Balance" << endl;
+        cout << " 4. Main Menu" << endl;
+        cout << "_____________________" << endl;
 
+        int command;
+
+        cout << "Enter the command: ";
+        cin >> command;
+
+        switch(command)
+        {
+        case 1:
+            Deposit();
+            user_menu();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            main_menu();
+            break;
+        default:
+            cout << "Wrong  input" << endl;
+
+            return user_menu();
+        }
+    }
     void main_menu()
     {
         cout << "_____________________" << endl;
-        cout << "MAIN MENU" << endl;
+        cout << "     MAIN MENU" << endl;
         cout << "1. Create Account" << endl;
         cout << "2. Login to Account" << endl;
         cout << "3. Exit" << endl;
         cout << "_____________________" << endl;
         int scommand;
+
         cout << "Enter the command: ";
         cin >> scommand;
 
@@ -100,13 +151,17 @@ class ATM
             main_menu();
             break;
         case 2:
-            LoginAccount();
+            if (LoginAccount() == 0)
+                user_menu();
+            else
+                main_menu();
             break;
         case 3:
             cout << "Exiting..." << endl;
             break;
         default:
             cout << "Wrong input" << endl;
+            return main_menu();
         }
     }
 };
