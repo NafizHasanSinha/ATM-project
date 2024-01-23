30f864ac8ac2f7bfe0796735d643723a65dd2198
NafizHasanSinha <154341736+NafizHasanSinha@users.noreply.github.com>
2024-01-23 12:16:45 +0600

    multiple account create & account CardNo and pin size
    Convert the number to a string
    Get string length to count digits
    Return digit count

--- Project_ATM.cpp
+++ Project_ATM.cpp
@@ -20,14 +20,20 @@ class ATM
     int z;
     int c;
 
-    public:
-        ATM()
+public:
+    ATM()
     {
         head = NULL;
         x = "";
         y = 0;
         z = 0;
         c = 0;
+    }
+    int count_digits(long long int n)
+    {
+
+        string num = to_string(n);
+        return num.size();
     }
     void Account()
     {
@@ -41,6 +47,11 @@ class ATM
 
             cout << "Please enter your Card No: ";
             cin >> y;
+            while(count_digits(y) < 11 || count_digits(y) > 11)
+            {
+                cout<<"Put 11 digits cardNo: "<<count_digits(y);
+                cin>>y;
+            }
             newnode->cardNo = y;
 
             cout << "Please enter your Pin: ";
@@ -54,6 +65,43 @@ class ATM
             head = newnode;
 
             cout << "Your Account Created" << endl;
+        }
+        else
+        {
+            node*newnode=new node;
+            cout<<"Please Enter your name: ";
+            cin>>x;
+            newnode->name=x;
+
+            cout<<"Please enter your Card No: ";
+            cin>>y;
+            while(count_digits(y) < 11 || count_digits(y) > 11)
+            {
+                cout<<"Put 11 digits cardNo: "<<count_digits(y);
+                cin>>y;
+            }
+            newnode->cardNo=y;
+
+            cout<<"Please enter your Pin: ";
+            cin>>z;
+            while(count_digits(z) < 4 || count_digits(z) > 4)
+            {
+                cout<<"Put 4 digits pin: ";
+                cin>>z;
+            }
+            newnode->pin=z;
+
+            newnode->cash=0;
+
+            newnode->next=NULL;
+            node*temp=head;
+            while(temp->next!=NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next=newnode;
+            newnode->prev=temp;
+            cout<<"Your Account created"<<endl;
         }
     }
 
